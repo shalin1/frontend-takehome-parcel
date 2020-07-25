d102a1fa7204615d55a8280d13d5105f87f0ba2e
Shalin Scupham <shalin.scupham@gmail.com>
2020-07-25 12:00:55 -0400

    Integrate search box in to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import SearchInput from './components/SearchInput'
 
-const App = () => <h1>✨👋🌍Hello World</h1>
+const App = () => {
+    const [searchTerm,setSearchTerm] = useState('')
+
+    return (
+        <>
+            <h1>✨👋🌍Hello World</h1>
+            <SearchInput value={searchTerm} onChange={setSearchTerm}/>
+        </>
+    )
+}
 
 export default App
--- test/App.test.js
+++ test/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../src/App';
+import SearchInput from '../src/components/SearchInput';
 
 describe('App', () => {
     const app = shallow(<App />);
@@ -9,4 +10,20 @@ describe('App', () => {
         expect(app.find('h1').exists()).toBe(true);
         expect(app.text()).toMatch(/Hello World/)
     });
+
+    it('renders a single search input', () => {
+        const input = app.find(SearchInput)
+        expect(input.length).toBe(1);
+    })
+
+    it('passes the correct props to the search input', () => {
+        const input = app.find(SearchInput)
+        expect(input.props()).toEqual({onChange: expect.any(Function), value: ''})
+    })
+
+    it("persists changes to the input's value", () => {
+        const wrapper = mount(<App/>)
+        wrapper.find('input').simulate('change', { target: { value: 'new value' } })
+        expect(wrapper.find('input').props().value).toBe('new value')
+    })
 });
