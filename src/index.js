3b1d48d468b91f1b0d2654fb0b851842688e2600
Shalin Scupham <shalin.scupham@gmail.com>
2020-07-26 13:45:36 -0400

    Add reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import App from './App';
+import './base.css'
 
 if (process.env.NODE_ENV !== 'production') {
     import('react-axe').then(axe => {
