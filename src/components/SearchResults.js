6f00ca92899d7e59453acd09791b6226588cc0f6
Shalin Scupham <shalin.scupham@gmail.com>
2020-07-27 16:24:15 -0400

    no gems found state

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -14,14 +14,16 @@ const SearchResults = ({foundGems,loading,savedGems,toggleSave}) => {
         <SearchResultsContainer>
             <wired-card elevation={3}>
             <h2>Search Results</h2>
-                {loading ? <p>loading...</p> :
-                <>
-                    {gems.map(gem => (
-                        <>
-                            <SearchResult key={gem.sha} gem={gem} toggleSave={toggleSave}/>
-                       </>
-                    ))}
-                </>
+                {loading ? <p>loading...</p>
+                    : gems.length === 0 ? <p>'💣☹️ no gems found️</p>
+                        : <>
+                            {gems.map(gem => (
+                                <>
+                                    <SearchResult key={gem.sha} gem={gem} toggleSave={toggleSave}/>
+                               </>
+                            ))}
+                        </>
+
             }
             </wired-card>
         </SearchResultsContainer>
